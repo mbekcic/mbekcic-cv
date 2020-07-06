8f511bbce5666ae30de6c270b63a687557a9d5b3
mbekcic <mbekcic42@gmail.com>
2020-07-06 10:45:01 +0200

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProjectsComponent } from './projects/projects.component';
+import { IntroComponent } from './intro/intro.component';
+import { CompetitionsComponent } from './competitions/competitions.component';
+import { EducationComponent } from './education/education.component';
+import { ContactComponent } from './contact/contact.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'projects', component: ProjectsComponent },
+  { path: '', component: IntroComponent },
+  { path: 'competitions', component: CompetitionsComponent },
+  { path: 'education', component: EducationComponent },
+  { path: 'contact', component: ContactComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { EducationComponent } from './education/education.component';
+import { CompetitionsComponent } from './competitions/competitions.component';
+import { IntroComponent } from './intro/intro.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectsComponent,
+    EducationComponent,
+    CompetitionsComponent,
+    IntroComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
